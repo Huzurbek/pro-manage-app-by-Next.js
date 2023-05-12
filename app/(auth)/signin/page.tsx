d8b71d359fd8ef7146a1bc880a77ee117b7e3fbf
Huzurbek <huzurbek@yahoo.com>
2023-05-12 17:10:22 +0500

    create reusable component

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,10 @@
+// import AuthForm from "@/components/Authform";
+
+export default function Signin() {
+  return (
+    <div>
+      sign in component
+      {/* <AuthForm mode="signin" /> */}
+    </div>
+  );
+}
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,23 @@
+import "@/styles/global.css";
+import GlassPane from "@/components/GlassPane";
+import Sidebar from "@/components/Sidebar";
+import { ReactNode } from "react";
+
+export default function DashboardRootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body className="h-screen w-screen candy-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center">
+          <Sidebar />
+          {children}
+        </GlassPane>
+        <div id="modal"></div>
+      </body>
+    </html>
+  );
+}
